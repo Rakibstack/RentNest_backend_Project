cb702cacb992c2152f86cee96cb8132c44dd4bc9
Rakibstack <rh6039511@gmail.com>
2026-07-27 20:35:23 +0600

    optimizing payment checkout session

--- src/module/payment/payment.service.ts
+++ src/module/payment/payment.service.ts
@@ -1,5 +1,7 @@
 import { RentalRequestStatus } from "../../../generated/prisma/enums";
+import config from "../../config";
 import { prisma } from "../../lib/prisma";
+import { stripe } from "../../lib/stripe";
 
 const createPaymentSession = async (
   userId: string,
@@ -24,15 +26,34 @@ const createPaymentSession = async (
   if (rentalRequest.payment) {
     throw new Error("Payment already completed.");
   }
-
   const amount = rentalRequest.agreedRent ?? rentalRequest.property.rent;
 
+  const session = await stripe.checkout.sessions.create({
+    mode: "payment",
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          unit_amount: amount * 100,
+          product_data: {
+            name: rentalRequest.property.title,
+          },
+          
+        },
+        quantity: 1,
+
+      },
+    ],
+    success_url: `${config.app_url}/payment-success`,
+    cancel_url : `${config.app_url}/payment-cancel`
+  });
+
   return {
-    rentalRequest,
-    amount
-  }
+     checkoutUrl : session.url
+  };
 };
 
 export const paymentService = {
-    createPaymentSession
-}
+  createPaymentSession,
+};
